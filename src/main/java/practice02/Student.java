a8bda51a4801f9844b08dd5057b7f17cca4108cb
TYG <793557590@qq.com>
2019-07-11 22:39:38 +0800

    test: pass first and second tests of practice02

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,24 @@
 package practice02;
 
-public class Student {
+public class Student extends Person{
+    private int klass;
+
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String introduce(){
+        return super.introduce();
+    }
+
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
 }
