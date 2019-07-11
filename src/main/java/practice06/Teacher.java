2daf06ddbc4db2b4e1b04538efb180a210a51a7d
TYG <793557590@qq.com>
2019-07-12 00:19:45 +0800

    pass the last of test of practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,28 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce(){
+        String last = klass == 0 ? " I teach No Class." : String.format(" I teach Class %d.",klass);
+        return String.format("%s I am a Teacher.%s",super.introduce(),last);
+    }
+
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
 }
